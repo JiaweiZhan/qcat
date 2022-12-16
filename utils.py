2c52a053dba1a581ddf42959e95734ffb8df4bad
jiaweizhan <jackzjw123@163.com>
2022-12-16 13:40:28 -0600

    multikpoint

--- utils.py
+++ utils.py
@@ -2,24 +2,48 @@ from xml.dom import minidom
 import numpy as np
 import datetime
 from tqdm import tqdm
+import os, time
 
 def parse_QE_XML(file_name):
     file = minidom.parse(file_name)
-    eigens_node = file.getElementsByTagName('eigenvalues')
-    eigenvalues = eigens_node[0].firstChild.data
-    eigenvalues = [float(num) * 27.2114 for num in eigenvalues.split()]
+    # num of KS states
+    nbnd_nodes = file.getElementsByTagName('nbnd')
+    nbnd = int(nbnd_nodes[0].firstChild.data)
 
-    occ_nodes = file.getElementsByTagName('occupations')
-    occupations = occ_nodes[1].firstChild.data
-    occupations = [float(num) for num in occupations.split()]
+    # num of kpoints
+    nks_nodes = file.getElementsByTagName('nks')
+    nks = int(nks_nodes[0].firstChild.data)
+    eigenvalues, occupations, weights = [], [], []
 
+    ks_nodes = file.getElementsByTagName('ks_energies')
+    for ks_node in ks_nodes:
+        # kpoint weight
+        k_point_node = ks_node.getElementsByTagName('k_point')
+        weight_ = float(k_point_node[0].attributes['weight'].value)
+        weights.append(weight_)
+
+        # eigenvalues
+        eigens_node = ks_node.getElementsByTagName('eigenvalues')
+        eigenvalue_ = eigens_node[0].firstChild.data
+        eigenvalue_ = [float(num) * 27.2114 for num in eigenvalue_.split()]
+        eigenvalues.append(eigenvalue_)
+
+        # occupation
+        occ_nodes = ks_node.getElementsByTagName('occupations')
+        occupation_ = occ_nodes[0].firstChild.data
+        occupation_ = [float(num) for num in occupation_.split()]
+        occupations.append(occupation_)
+
+    # fft grids
     fft_nodes = file.getElementsByTagName('fft_grid')
     np0v = int(fft_nodes[0].attributes['nr1'].value)
     np1v = int(fft_nodes[0].attributes['nr2'].value)
     np2v = int(fft_nodes[0].attributes['nr3'].value)
-    print(f"FFTW3: {[np0v, np1v, np2v]}")
-    out_dict = {'eigen': eigenvalues,
-                'occ': occupations,
+    out_dict = {'nks': nks,
+                'kweights': np.array(weights),
+                'nbnd': nbnd,
+                'eigen': np.array(eigenvalues),
+                'occ': np.array(occupations),
                 'fftw': [np0v, np1v, np2v]}
     return out_dict 
 
@@ -56,7 +80,7 @@ def parse_QE_wfc(file_name):
 
         evc = np.zeros( (nbnd, npol*igwx), dtype="complex128")
         f.seek(8,1)
-        for i in range(nbnd):
+        for i in tqdm(range(nbnd), desc='read wfc bands'):
             evc[i,:] = np.fromfile(f, dtype='complex128', count=npol*igwx)
             f.seek(8, 1)
 
@@ -78,10 +102,11 @@ def parse_QE_wfc(file_name):
 z_axis_assign = []
 lcbm = lvbm = None
 lock_list = condition_list = None
-evc_r = None
-numOcc = None
+ksStateZAve = None
+numOcc = None               # [nks * nbnds]
+kWeights = None
 tot_bands = None
-eigens = None
+eigens = None               # [nks * nbnds]
 delta = None
 
 def ldos_worker(id):
@@ -100,64 +125,83 @@ def ldos_worker(id):
 
     if id == 0:
         for z in tqdm(z_axis, desc='compute LDOS'):
-            # evc_r = [ibnd, x, y, z]
-            preFactor = np.sum(np.absolute(evc_r[:, :, :, z]) ** 2, axis=(1, 2))
-            sumVBTot = np.sum(preFactor[:numOcc])
+            # ksStateZAve: [ik, ibnd, z]
+            preFactor = ksStateZAve[:, :, z]
+            sumVBTot = np.sum(preFactor * numOcc * kWeights[:, np.newaxis])
 
-            min_arg = numOcc - 1
-            max_arg = numOcc
+            KSEnergyTot = []
+            KSFactorTot = []
+            for i in range(eigens.shape[0]):
+                KSEnergyTot.extend(eigens[i])
+                KSFactorTot.extend(preFactor[i] * kWeights[i])
+
+            zipEneFac = zip(KSEnergyTot, KSFactorTot)
+            eneSort, facSort = list(zip(*sorted(zipEneFac, key=lambda x: x[0])))
+
+            min_arg = int(np.sum(numOcc)) - 1
+            max_arg = int(np.sum(numOcc))
 
             sumLeft = 0
             while min_arg >= 0:
-                sumLeft += preFactor[min_arg]
+                sumLeft += facSort[min_arg]
                 if sumLeft >= sumVBTot * delta:
                     break
                 else:
                     min_arg -= 1
-            if min_arg != numOcc -1:
+            if min_arg != int(np.sum(numOcc)) -1:
                 min_arg += 1
 
             sumRight = 0
-            while max_arg <= tot_bands - 1:
-                sumRight += preFactor[max_arg]
+            while max_arg <= len(eneSort) - 1:
+                sumRight += facSort[max_arg]
                 if sumRight >= sumVBTot * delta:
                     break
                 else:
                     max_arg += 1
-            if max_arg != numOcc:
+            if max_arg != int(np.sum(numOcc)):
                 max_arg -= 1
-            lvbm[z] = eigens[min_arg]
-            lcbm[z] = eigens[max_arg]
+            lvbm[z] = eneSort[min_arg]
+            lcbm[z] = eneSort[max_arg]
     else:
         for z in z_axis:
-            # evc_r = [ibnd, x, y, z]
-            preFactor = np.sum(np.absolute(evc_r[:, :, :, z]) ** 2, axis=(1, 2))
-            sumVBTot = np.sum(preFactor[:numOcc])
+            # ksStateZAve: [ik, ibnd, z]
+            preFactor = ksStateZAve[:, :, z]
+            sumVBTot = np.sum(preFactor * numOcc * kWeights[:, np.newaxis])
 
-            min_arg = numOcc - 1
-            max_arg = numOcc
+            KSEnergyTot = []
+            KSFactorTot = []
+            for i in range(eigens.shape[0]):
+                KSEnergyTot.extend(eigens[i])
+                KSFactorTot.extend(preFactor[i] * kWeights[i])
+
+            zipEneFac = zip(KSEnergyTot, KSFactorTot)
+            eneSort, facSort = list(zip(*sorted(zipEneFac, key=lambda x: x[0])))
+
+
+            min_arg = int(np.sum(numOcc)) - 1
+            max_arg = int(np.sum(numOcc))
 
             sumLeft = 0
             while min_arg >= 0:
-                sumLeft += preFactor[min_arg]
+                sumLeft += facSort[min_arg]
                 if sumLeft >= sumVBTot * delta:
                     break
                 else:
                     min_arg -= 1
-            if min_arg != numOcc -1:
+            if min_arg != int(np.sum(numOcc)) -1:
                 min_arg += 1
 
             sumRight = 0
-            while max_arg <= tot_bands - 1:
-                sumRight += preFactor[max_arg]
+            while max_arg <= len(eneSort) - 1:
+                sumRight += facSort[max_arg]
                 if sumRight >= sumVBTot * delta:
                     break
                 else:
                     max_arg += 1
-            if max_arg != numOcc:
+            if max_arg != int(np.sum(numOcc)):
                 max_arg -= 1
-            lvbm[z] = eigens[min_arg]
-            lcbm[z] = eigens[max_arg]
+            lvbm[z] = eneSort[min_arg]
+            lcbm[z] = eneSort[max_arg]
     return
 
 def time_now():
@@ -171,9 +215,73 @@ def time_now():
     print("=====================================================")
     return
 
+def storeGvec(xml_data, wfc_data, realSpace=True, Store=False, storeFolder=None):
+    fft_grid = xml_data['fftw']
+    fft_grid = np.array(fft_grid) // 2 + 1
+    if not Store:
+        # store everyting in memory
+        evc_g = np.zeros([wfc_data['nbnd'], fft_grid[0], fft_grid[1], fft_grid[2]], dtype=np.complex128)
+        for index, gvec in enumerate(tqdm(wfc_data['mill'], desc='store wfc_g')):
+            eig = wfc_data['evc'][:, index]
+            evc_g[:, gvec[0], gvec[1], gvec[2]] = eig
+            if (gvec[0] != 0 or gvec[1] != 0 or gvec[2] != 0):
+                evc_g[:, -gvec[0], -gvec[1], -gvec[2]] = np.conj(eig)
+        if not realSpace:
+            return evc_g
+        else:
+            evc_r = np.fft.ifftn(evc_g, axes=(1, 2, 3,), norm='forward')
+            return evc_r
+    else:
+        # memory is not enough, store file under folder for further loading
+        assert(storeFolder is not None)
+        isExist = os.path.exists(storeFolder)
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs(storeFolder)
+        millIndex = wfc_data['mill']
+        allZeroIndex = -1
+        for index in range(millIndex.shape[0]):
+            if millIndex[index, 0] == 0 and millIndex[index, 1] == 0 and millIndex[index, 2] == 0:
+                allZeroIndex = index
+                break
+        if allZeroIndex != -1:
+            millIndex = np.delete(millIndex, allZeroIndex, axis=0)
+
+        for ibnd in tqdm(range(wfc_data['nbnd']), desc='store ibnd'):
+            evc_g = np.zeros([fft_grid[0], fft_grid[1], fft_grid[2]], dtype=np.complex128)
+            wfc_slice = list(wfc_data['evc'][ibnd, :])
+            # for index, gvec in enumerate(wfc_data['mill']):
+            #     evc_g[gvec[0], gvec[1], gvec[2]] = wfc_data['evc'][ibnd, index]
+            #     if (gvec[0] != 0 or gvec[1] != 0 or gvec[2] != 0):
+            #         evc_g[-gvec[0], -gvec[1], -gvec[2]] = np.conj(wfc_data['evc'][ibnd, index])
+            evc_g[wfc_data['mill'][:, 0], wfc_data['mill'][:, 1], wfc_data['mill'][:, 2]] = wfc_data['evc'][ibnd, :]
+            if allZeroIndex != -1:
+                del wfc_slice[allZeroIndex]
+            evc_g[ - millIndex[:, 0], - millIndex[:, 1], - millIndex[:, 2]] = np.conj(wfc_slice)
+            evc_r = None
+            if not realSpace:
+                wfcName = 'wfc_g_' + str(ibnd + 1).zfill(5)
+                np.save(storeFolder + '/' + wfcName, evc_g)
+            else:
+                evc_r = np.fft.ifftn(evc_g, norm='forward')
+                wfcName = 'wfc_r_' + str(ibnd + 1).zfill(5)
+                np.save(storeFolder + '/' + wfcName, evc_r)
+        return
+
 
 if __name__=="__main__":
-    xml_data = parse_QE_XML('./data-file-schema.xml')
-    wfc_data = parse_QE_wfc('./wfc1.dat')
-    print(wfc_data["ngw"])
-    print(xml_data["occ"])
+    # get the start time
+    st = time.time()
+
+    xml_data = parse_QE_XML('./si_si3n4.save/data-file-schema.xml')
+    # wfc_data = parse_QE_wfc('./si_si3n4.save/wfc1.dat')
+    # storeGvec(xml_data, wfc_data, realSpace=True, Store=True, storeFolder='./wfc1/')
+    # print(wfc_data["ngw"])
+    # print(xml_data["occ"])
+
+    # get the end time
+    et = time.time()
+
+    # get the execution time
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
