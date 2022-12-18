61e01f1d78a2f16217e667b64923c4d0c6fe3b1e
jiaweizhan <jackzjw123@163.com>
2022-12-17 21:16:34 -0600

    class

---
+++ class_ldos.py
@@ -0,0 +1,249 @@
+import numpy as np
+from tqdm import tqdm
+import os, time
+import threading
+import qe_io
+import shutil
+
+class LDOS:
+    # thread-related global value
+    z_axis_assign = []
+    lock_list = threading.Lock()
+    condition_list = threading.Condition(lock_list)
+    lcbm = None
+    lvbm = None
+    ksStateZAve = None
+    numOcc = None               # [nks * nbnd]
+    kWeights = None
+    eigens = None               # [nks * nbnd]
+    delta = None
+    numThread = None
+    nspin = None
+    nks = None
+    nbnd = None
+    saveFolder = None
+    storeFolder = None
+    backup = None
+
+    def __init__(self, delta, numThread, saveFolder, backup):
+        """
+        Init LDOS class:
+            param:
+                delta: float;
+                numThread: int;
+                saveFolder: str
+                backp: bool
+        """
+        self.delta = delta
+        self.numThread = numThread
+        self.ksStateZAve = None
+        self.lvbm = None 
+        self.lcbm = None 
+        self.saveFolder = saveFolder
+        self.backup = backup
+
+    def ldos_worker(self, id):
+        """
+        worker to compute LCBM and LVBM for multiple 
+        z axis in z_axis_assign in a multi-threaded way
+            param:
+                id: int
+        """
+        self.lock_list.acquire()
+        while len(self.z_axis_assign) == 0:
+            self.condition_list.wait()
+        z_axis = self.z_axis_assign.pop(0)
+        # poison
+        if len(z_axis) == 0:
+            self.z_axis_assign.append([])
+            self.lock_list.release()
+            return
+        # not a poison
+        self.lock_list.release()
+
+        if id == 0:
+            for z in tqdm(z_axis, desc='compute LDOS'):
+                # ksStateZAve: [ispin, ik, ibnd, z]
+                preFactor = self.ksStateZAve[:, :, :, z]
+                sumVBTot = np.sum(preFactor * self.numOcc * self.kWeights[np.newaxis, :, np.newaxis])
+
+                KSEnergyTot = []
+                KSFactorTot = []
+                for i in range(self.nspin):
+                    for j in range(self.nks):
+                        KSEnergyTot.extend(self.eigens[i, j])
+                        KSFactorTot.extend(preFactor[i, j] * self.kWeights[j])
+
+                zipEneFac = zip(KSEnergyTot, KSFactorTot)
+                eneSort, facSort = list(zip(*sorted(zipEneFac, key=lambda x: x[0])))
+
+                min_arg = int(np.sum(self.numOcc)) - 1
+                max_arg = int(np.sum(self.numOcc))
+
+                sumLeft = 0
+                while min_arg >= 0:
+                    sumLeft += facSort[min_arg]
+                    if sumLeft >= sumVBTot * self.delta:
+                        break
+                    else:
+                        min_arg -= 1
+                if min_arg != int(np.sum(self.numOcc)) -1:
+                    min_arg += 1
+
+                sumRight = 0
+                while max_arg <= len(eneSort) - 1:
+                    sumRight += facSort[max_arg]
+                    if sumRight >= sumVBTot * self.delta:
+                        break
+                    else:
+                        max_arg += 1
+                if max_arg != int(np.sum(self.numOcc)):
+                    max_arg -= 1
+                self.lvbm[z] = eneSort[min_arg]
+                self.lcbm[z] = eneSort[max_arg]
+        else:
+            for z in z_axis:
+                # ksStateZAve: [ispin, ik, ibnd, z]
+                preFactor = self.ksStateZAve[:, :, :, z]
+                sumVBTot = np.sum(preFactor * self.numOcc * self.kWeights[np.newaxis, :, np.newaxis])
+
+                KSEnergyTot = []
+                KSFactorTot = []
+                for i in range(self.nspin):
+                    for j in range(self.nks):
+                        KSEnergyTot.extend(self.eigens[i, j])
+                        KSFactorTot.extend(preFactor[i, j] * self.kWeights[j])
+
+                zipEneFac = zip(KSEnergyTot, KSFactorTot)
+                eneSort, facSort = list(zip(*sorted(zipEneFac, key=lambda x: x[0])))
+
+                min_arg = int(np.sum(self.numOcc)) - 1
+                max_arg = int(np.sum(self.numOcc))
+
+                sumLeft = 0
+                while min_arg >= 0:
+                    sumLeft += facSort[min_arg]
+                    if sumLeft >= sumVBTot * self.delta:
+                        break
+                    else:
+                        min_arg -= 1
+                if min_arg != int(np.sum(self.numOcc)) -1:
+                    min_arg += 1
+
+                sumRight = 0
+                while max_arg <= len(eneSort) - 1:
+                    sumRight += facSort[max_arg]
+                    if sumRight >= sumVBTot * self.delta:
+                        break
+                    else:
+                        max_arg += 1
+                if max_arg != int(np.sum(self.numOcc)):
+                    max_arg -= 1
+                self.lvbm[z] = eneSort[min_arg]
+                self.lcbm[z] = eneSort[max_arg]
+        return
+
+    def computeLDOS(self):
+        wfc_files = [self.saveFolder + '/' + file for file in os.listdir(self.saveFolder) if 'wfc' in file]
+
+        # xml_data = utils.parse_QE_XML(xml_file)
+        # # num of occupied bands
+        # nks = xml_data['nks']
+        # nspin = xml_data['nspin']
+        # utils.numOcc = xml_data['occ']   # [nspin * nks * nbnds]
+        # utils.kWeights = xml_data['kweights']
+        # utils.tot_bands = xml_data['nbnd']
+        # utils.eigens = xml_data['eigen'] # [nspin * nks * nbnds]
+        # fft_grid = xml_data['fftw']
+        # fft_grid = np.array(fft_grid) // 2 + 1
+        qe = qe_io.QERead(numThread=self.numThread)
+        xml_data = qe.parse_QE_XML(self.saveFolder + '/data-file-schema.xml')
+        self.numOcc = xml_data['occ']
+        self.kWeights = xml_data['kweights']
+        self.nbnd = xml_data['nbnd']
+        self.eigens = xml_data['eigen']
+        self.nspin = self.numOcc.shape[0]
+        self.nks = self.numOcc.shape[1]
+        self.nbnd = self.numOcc.shape[2]
+        fft_grid = xml_data['fftw']
+        fft_grid = np.array(fft_grid) // 2 + 1
+
+        self.ksStateZAve = np.zeros((self.nspin, self.nks, self.nbnd, fft_grid[2]))
+        for index in range(self.nks * self.nspin):
+            wfc_data = qe.parse_QE_wfc(wfc_files[index])
+            ik = wfc_data['ik']
+            ispin = wfc_data['ispin']
+            print(f"ik: {ik}, ispin: {ispin}, nbnd: {wfc_data['nbnd']}, npol: {wfc_data['npol']}, igwx: {wfc_data['igwx']}")
+
+            # store and read
+            if self.backup == 1:
+                storeFolder = wfc_files[index].split('/')[-1].split('.')[0]
+                self.storeFolder = storeFolder
+                qe.computeWFC(Store=True, storeFolder=storeFolder)
+                wfcStored = os.listdir(storeFolder)
+                for fileName in tqdm(wfcStored, desc='read wfc from stored file'):
+                    wfcName = storeFolder + '/' + fileName
+                    ibnd = int(fileName.split('.')[0].split('_')[-1])
+                    evc_r = np.load(wfcName)
+                    self.ksStateZAve[ispin - 1, ik - 1, ibnd - 1, :] = np.sum(np.absolute(evc_r) ** 2, axis=(0, 1,))
+                shutil.rmtree(storeFolder)
+            else:
+                # direct comput and store in memory
+                evc_r = qe.storeGvec(Store=False)
+                self.ksStateZAve[ispin - 1, ik - 1, :, :] = np.sum(np.absolute(evc_r) ** 2, axis=(1, 2,))
+            print('\n')
+
+        self.lcbm = np.zeros(fft_grid[2])
+        self.lvbm = np.zeros(fft_grid[2])
+        #define lock and condition
+        self.lock_list = threading.Lock()
+        self.condition_list = threading.Condition(self.lock_list)
+
+        num_thread = self.numThread
+        length_chunk = fft_grid[2] / num_thread
+        a_axis_tot = list(range(fft_grid[2]))
+
+        self.lock_list.acquire()
+        for i in range(num_thread):
+            self.z_axis_assign.append(a_axis_tot[int(i * length_chunk) : int((i + 1) * length_chunk)])
+
+        # poison
+        self.z_axis_assign.append([])
+        self.condition_list.notify_all()
+        self.lock_list.release()
+
+        # start thread
+        threads = []
+        ids = []
+        for i in range(num_thread):
+            ids.append(i)
+            thread_id = threading.Thread(target=self.ldos_worker, args = (ids[i],))
+            threads.append(thread_id)
+
+        for thread_id in threads:
+            thread_id.start()
+
+        for thread_id in threads:
+            thread_id.join()
+
+    def localBandEdge(self):
+        return self.lcbm, self.lvbm
+
+if __name__=="__main__":
+    # get the start time
+    st = time.time()
+
+    numThread = 15
+    qe = qe_io.QERead(numThread)
+    qe.parse_QE_XML('../si_si3n4_gamma.save/data-file-schema.xml')
+    qe.parse_QE_wfc('../si_si3n4_gamma.save/wfc1.dat')
+    qe.computeWFC(realSpace=True, Store=True, storeFolder='./wfc1/')
+    # print(wfc_data["ngw"])
+    # print(xml_data["occ"])
+
+    # get the end time
+    et = time.time()
+
+    # get the execution time
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
---
+++ qe_io.py
@@ -0,0 +1,277 @@
+from xml.dom import minidom
+import numpy as np
+from tqdm import tqdm
+import threading
+import os
+
+class QERead:
+    storeIbndList = []
+    lock_Ibnds = threading.Lock()
+    iBnds_condition = threading.Condition(lock_Ibnds)
+    xml_data = None
+    wfc_data = None
+    threadNum = None
+
+    def __init__(self, numThread):
+        self.threadNum = numThread
+
+    def parse_QE_XML(self, file_name):
+        """
+        analyze QE data-file-schema.xml
+            param:
+                file_name: str
+            return:
+                 dict of {'nks', 'kweights', 'nbnd', 'eigen', 'occ', 'fftw'}
+        """
+        # unit convert
+        hartree2ev = 27.2114
+        file = minidom.parse(file_name)
+        # spin
+        spin_nodes = file.getElementsByTagName('spin')
+        lsda_nodes = spin_nodes[0].getElementsByTagName('lsda')
+        nspin = 1
+        if lsda_nodes[0].firstChild.data == 'true':
+            nspin = 2
+
+
+        # num of KS states
+        nbnd_up, nbnd_dw, nbnd = -1, -1, -1
+        nbnd_nodes = file.getElementsByTagName('nbnd')
+        try: 
+            nbnd = int(nbnd_nodes[0].firstChild.data)
+        except IndexError:
+            pass
+        if nspin == 2:
+            nbnd_up_nodes = file.getElementsByTagName('nbnd_up')
+            nbnd_up = int(nbnd_up_nodes[0].firstChild.data)
+            nbnd_dw_nodes = file.getElementsByTagName('nbnd_dw')
+            nbnd_dw = int(nbnd_dw_nodes[0].firstChild.data)
+            if nbnd != -1:
+                assert(nbnd == nbnd_dw and nbnd == nbnd_up)
+            else:
+                assert(nbnd_up == nbnd_dw)
+                nbnd = nbnd_dw
+
+        # fermi energy
+        fermi_nodes = file.getElementsByTagName('fermi_energy')
+        fermiEne = float(fermi_nodes[0].firstChild.data) * hartree2ev
+
+        # num of kpoints
+        nks_nodes = file.getElementsByTagName('nks')
+        nks = int(nks_nodes[0].firstChild.data)
+     
+        # [nspin * nks * nbnd]
+        eigenvalues, occupations, weights = np.zeros((nspin, nks, nbnd)), np.zeros((nspin, nks, nbnd)), np.zeros(nks)
+
+        ks_nodes = file.getElementsByTagName('ks_energies')
+        for index, ks_node in enumerate(ks_nodes):
+            # kpoint weight
+            k_point_node = ks_node.getElementsByTagName('k_point')
+            weights[index] = float(k_point_node[0].attributes['weight'].value)
+
+            # eigenvalues
+            eigens_node = ks_node.getElementsByTagName('eigenvalues')
+            eigenvalue_ = eigens_node[0].firstChild.data
+            eigenvalue_ = [float(num) * hartree2ev for num in eigenvalue_.split()]
+            for ispin in range(nspin):
+                eigenvalues[ispin, index, :] = eigenvalue_[ispin * nbnd : (ispin + 1) * nbnd]
+
+            # occupation
+            # occ_nodes = ks_node.getElementsByTagName('occupations')
+            # occupation_ = occ_nodes[0].firstChild.data
+            # occupation_ = [float(num) for num in occupation_.split()]
+            # for ispin in range(nspin):
+            #     occupations[ispin, index, :] = occupation_[ispin * nbnd : (ispin + 1) * nbnd]
+
+            # occupation another way
+            occupations[eigenvalues <= fermiEne] = 1
+            occupations[eigenvalues > fermiEne] = 0
+
+        # fft grids
+        fft_nodes = file.getElementsByTagName('fft_grid')
+        np0v = int(fft_nodes[0].attributes['nr1'].value)
+        np1v = int(fft_nodes[0].attributes['nr2'].value)
+        np2v = int(fft_nodes[0].attributes['nr3'].value)
+        out_dict = {'nspin': nspin,
+                    'nks': nks,
+                    'fermi': fermiEne,
+                    'kweights': weights,
+                    'nbnd': nbnd,
+                    'eigen': eigenvalues,
+                    'occ': occupations,
+                    'fftw': [np0v, np1v, np2v]}
+        self.xml_data = out_dict
+        return out_dict 
+
+    def parse_QE_wfc(self, file_name):
+        """
+        analyze QE wfc*.dat 
+            param:
+                file_name: str
+            return:
+                 dict of {'ik', 'xk', 'ispin', 'nbnd', 'ngw', 'evc'...}
+        """
+        with open(file_name, 'rb') as f:
+            # Moves the cursor 4 bytes to the right
+            f.seek(4)
+
+            ik = np.fromfile(f, dtype='int32', count=1)[0]
+            xk = np.fromfile(f, dtype='float64', count=3)
+            ispin = np.fromfile(f, dtype='int32', count=1)[0]
+            gamma_only = bool(np.fromfile(f, dtype='int32', count=1)[0])
+            scalef = np.fromfile(f, dtype='float64', count=1)[0]
+
+            # Move the cursor 8 byte to the right
+            f.seek(8, 1)
+
+            ngw = np.fromfile(f, dtype='int32', count=1)[0]
+            igwx = np.fromfile(f, dtype='int32', count=1)[0]
+            npol = np.fromfile(f, dtype='int32', count=1)[0]
+            nbnd = np.fromfile(f, dtype='int32', count=1)[0]
+
+            # Move the cursor 8 byte to the right
+            f.seek(8, 1)
+
+            b1 = np.fromfile(f, dtype='float64', count=3)
+            b2 = np.fromfile(f, dtype='float64', count=3)
+            b3 = np.fromfile(f, dtype='float64', count=3)
+
+            f.seek(8,1)
+            
+            mill = np.fromfile(f, dtype='int32', count=3*igwx)
+            mill = mill.reshape( (igwx, 3) ) 
+
+            evc = np.zeros( (nbnd, npol*igwx), dtype="complex128")
+            f.seek(8,1)
+            for i in tqdm(range(nbnd), desc='read wfc bands'):
+                evc[i,:] = np.fromfile(f, dtype='complex128', count=npol*igwx)
+                f.seek(8, 1)
+
+            wfc_dict = {'ik': ik,
+                        'xk': xk,
+                        'ispin': ispin,
+                        'gamma_only': gamma_only,
+                        'scalef': scalef,
+                        'ngw': ngw,
+                        'igwx': igwx,
+                        'npol': npol,
+                        'nbnd': nbnd,
+                        'mill': mill,
+                        'b': [b1, b2, b3],
+                        'evc': evc}
+        self.wfc_data = wfc_dict
+        return wfc_dict
+
+    def computeWFC(self, realSpace=True, Store=False, storeFolder=None):
+        """
+        Compute wavefunction in a multi-threaded way
+        by either storing wfc in memory or disk
+        param:
+            xml_data: return of parse_QE_XML
+            wfc_data: return of parse_QE_wfc
+            realSpace: bool, whether convert wfc to real space
+            Store: bool, whether store wfc on disk to save memory
+            storeFolder: str, whether the wfc is strored
+            threadNum: number of thread to store wfc
+        """
+        fft_grid = self.xml_data['fftw']
+        fft_grid = np.array(fft_grid) // 2 + 1
+        if not Store:
+            # store everyting in memory
+            evc_g = np.zeros([self.wfc_data['nbnd'], fft_grid[0], fft_grid[1], fft_grid[2]], dtype=np.complex128)
+            for index, gvec in enumerate(tqdm(self.wfc_data['mill'], desc='store wfc_g')):
+                eig = self.wfc_data['evc'][:, index]
+                evc_g[:, gvec[0], gvec[1], gvec[2]] = eig
+                if self.wfc_data['gamma_only']:
+                    if (gvec[0] != 0 or gvec[1] != 0 or gvec[2] != 0):
+                        evc_g[:, -gvec[0], -gvec[1], -gvec[2]] = np.conj(eig)
+            if not realSpace:
+                return evc_g
+            else:
+                evc_r = np.fft.ifftn(evc_g, axes=(1, 2, 3,), norm='forward')
+                return evc_r
+        else:
+            # memory is not enough, store file under folder for further loading
+            assert(storeFolder is not None)
+            isExist = os.path.exists(storeFolder)
+            if not isExist:
+                # Create a new directory because it does not exist
+                os.makedirs(storeFolder)
+            millIndex = self.wfc_data['mill']
+            allZeroIndex = -1
+            for index in range(millIndex.shape[0]):
+                if millIndex[index, 0] == 0 and millIndex[index, 1] == 0 and millIndex[index, 2] == 0:
+                    allZeroIndex = index
+                    break
+            if allZeroIndex != -1:
+                millIndex = np.delete(millIndex, allZeroIndex, axis=0)
+
+            self.lock_Ibnds.acquire()
+            self.storeIbndList = list(range(self.wfc_data['nbnd']))
+            self.storeIbndList.append(-1)
+            self.iBnds_condition.notify_all()
+            self.lock_Ibnds.release()
+            
+            threads = []
+            for _ in range(self.threadNum):
+                thread_id = threading.Thread(target=self.storeGWorker, args=(millIndex, fft_grid, allZeroIndex, storeFolder, realSpace,))
+                threads.append(thread_id)
+
+            for thread_id in threads:
+                thread_id.start()
+
+            for thread_id in threads:
+                thread_id.join()
+            
+            # TODO: multithread
+            # for ibnd in tqdm(range(wfc_data['nbnd']), desc='store ibnd'):
+            #     evc_g = np.zeros([fft_grid[0], fft_grid[1], fft_grid[2]], dtype=np.complex128)
+            #     wfc_slice = list(wfc_data['evc'][ibnd, :])
+            #     evc_g[wfc_data['mill'][:, 0], wfc_data['mill'][:, 1], wfc_data['mill'][:, 2]] = wfc_data['evc'][ibnd, :]
+            #     if allZeroIndex != -1:
+            #         del wfc_slice[allZeroIndex]
+            #     evc_g[ - millIndex[:, 0], - millIndex[:, 1], - millIndex[:, 2]] = np.conj(wfc_slice)
+            #     evc_r = None
+            #     if not realSpace:
+            #         wfcName = 'wfc_g_' + str(ibnd + 1).zfill(5)
+            #         np.save(storeFolder + '/' + wfcName, evc_g)
+            #     else:
+            #         evc_r = np.fft.ifftn(evc_g, norm='forward')
+            #         wfcName = 'wfc_r_' + str(ibnd + 1).zfill(5)
+            #         np.save(storeFolder + '/' + wfcName, evc_r)
+            return
+
+    def storeGWorker(self, millIndex, fft_grid, allZeroIndex, storeFolder, realSpace):
+        while True:
+            self.lock_Ibnds.acquire()
+            while len(self.storeIbndList) == 0:
+                self.iBnds_condition.wait()
+            iBnd = self.storeIbndList.pop(0)
+            # poison
+            if iBnd == -1:
+                self.storeIbndList.append(-1)
+                self.lock_Ibnds.release()
+                return
+            # not a poison
+            self.lock_Ibnds.release()
+
+            # for ibnd in tqdm(iBnds, desc='store ibnd'):
+            evc_g = np.zeros([fft_grid[0], fft_grid[1], fft_grid[2]], dtype=np.complex128)
+            wfc_slice = self.wfc_data['evc'][iBnd, :]
+            evc_g[self.wfc_data['mill'][:, 0], self.wfc_data['mill'][:, 1], self.wfc_data['mill'][:, 2]] = self.wfc_data['evc'][iBnd, :]
+            if allZeroIndex != -1:
+                wfc_slice = np.delete(wfc_slice, allZeroIndex)
+            if self.wfc_data['gamma_only']:
+                evc_g[- millIndex[:, 0], - millIndex[:, 1], - millIndex[:, 2]] = np.conj(wfc_slice)
+            evc_r = None
+            if not realSpace:
+                wfcName = 'wfc_g_' + str(iBnd + 1).zfill(5)
+                np.save(storeFolder + '/' + wfcName, evc_g)
+            else:
+                evc_r = np.fft.ifftn(evc_g, norm='forward')
+                wfcName = 'wfc_r_' + str(iBnd + 1).zfill(5)
+                np.save(storeFolder + '/' + wfcName, evc_r)
+
+    def info(self):
+        qe_data = self.xml_data | self.xml_data
+        return qe_data
