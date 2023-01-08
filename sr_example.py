21a6495d70441c5a9e7ad20585d7837142e48e3a
Jiawei Zhan <jiaweiz@midway3-0298.rcc.local>
2023-01-08 00:32:20 -0600

    sr component [dev]

--- sr_example.py
+++ sr_example.py
@@ -1,10 +1,44 @@
 from abinitioToolKit import qbox_io
+from abinitioToolKit import qe_io
 from abinitioToolKit import utils
 from mpi4py import MPI
-import time
+import argparse
 
 comm = MPI.COMM_WORLD
 
 if __name__ == "__main__":
-    qbox = qbox_io.QBOXRead(comm)
-    utils.local_contribution(qbox, info_name=None, wfc_name='../gs.gs.xml', comm=comm)
+    rank = comm.Get_rank()
+    if rank == 0:
+        utils.time_now()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--abinitio", type=str,
+            help="abinitio software: qe/qbox. Default: qbox")
+    parser.add_argument("-i", "--info", type=str,
+            help="file for basic info: data.xml from qe and none for qbox. Default: None")
+    parser.add_argument("-w", "--wfc", type=str,
+            help="file/folder for wfc --- qe: save folder; qbox: xml file. Default: ./gs.gs.xml")
+    args = parser.parse_args()
+
+    if not args.abinitio:
+        args.abinitio = "qbox"
+    if not args.info:
+        args.info = None 
+    if not args.wfc:
+        args.wfc = "./gs.gs.xml" 
+
+    if rank == 0:
+        print(f"configure:\
+                \n {''.join(['-'] * 41)}\
+                \n{'software':^20}:{args.abinitioRead:^20}\
+                \n{'info':^20}:{args.info:^20}\
+                \n{'wfc':^20}:{args.wfc:^20}\
+                \n {''.join(['-'] * 41)}\n\
+                ")
+
+    abinitioRead = None
+    if args.abinitio.lower() == "qbox":
+        abinitioRead = qbox_io.QBOXRead(comm)
+    elif args.abinitio.lower() == "qe":
+        abinitioRead = qe_io.QERead(comm)
+    utils.local_contribution(abinitioRead, info_name=args.info, wfc_name=args.wfc, comm=comm)
