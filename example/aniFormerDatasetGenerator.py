0b518d26b074c27d23a0ce160602358e6fe3b7cf
Jiawei Zhan <jiaweiz@midway3-login4.rcc.local>
2023-04-26 13:26:45 -0500

    aniformerDatasetGenerator [dev]

--- example/aniFormerDatasetGenerator.py
+++ example/aniFormerDatasetGenerator.py
@@ -37,15 +37,26 @@ if __name__ == "__main__":
             help="folder that store XML sample and qbox.out. Default: ../")
     parser.add_argument("-a", "--alphaFile", type=str,
             help="Local Dielectric Function File. Default: ../alpha.txt")
+    parser.add_argument("-n", "--material_name", type=str,
+            help="material_name. Default: sihwat")
+    parser.add_argument("-o", "--species_order", nargs='*',
+            help="species_order. Default: H O Si")
     args = parser.parse_args()
 
     if not args.saveFileFolder:
         args.saveFileFolder = "../" 
     if not args.alphaFile:
         args.alphaFile = "../alpha.txt" 
+    if not args.material_name:
+        args.material_name = "sihwat" 
+    if not args.species_order:
+        args.species_order =  ['H', 'O', 'Si']
+    material_name = args.material_name
 
     conf_tab = {"saveFileFolder": args.saveFileFolder,
                 "alphaFile": args.alphaFile,
+                "material_name": args.material_name,
+                "species_order": args.species_order,
                 "MPI size": comm.Get_size()}
     utils.print_conf(conf_tab)
 
@@ -87,7 +98,6 @@ if __name__ == "__main__":
     dataset_folder = './Dataset'
     structure_folder = 'structures'
     attribute_folder = 'attributes'
-    material_name = 'sihwat'
     if rank == 0:
         if not os.path.exists(dataset_folder):
             os.mkdir(dataset_folder)
@@ -95,6 +105,8 @@ if __name__ == "__main__":
             os.mkdir(os.path.join(dataset_folder, structure_folder))
         if not os.path.exists(os.path.join(dataset_folder, attribute_folder)):
             os.mkdir(os.path.join(dataset_folder, attribute_folder))
+        if not os.path.exists(os.path.join(dataset_folder, attribute_folder, material_name)):
+            os.mkdir(os.path.join(dataset_folder, attribute_folder, material_name))
 
     comm.Barrier()
 
@@ -104,7 +116,7 @@ if __name__ == "__main__":
                 'pbc': [True, True, True],
                 'atomic_positions': positions,
                 'species': species,
-                'species_order': ['H', 'O', 'Si'],
+                'species_order': args.species_order,
                 }
 
         point_gap = 5
@@ -124,8 +136,24 @@ if __name__ == "__main__":
     point_indices = np.array(point_indices)
 
     if rank == 0:
-        i_indices, j_indices, k_indices = point_indices[:, 0], point_indices[:, 1], point_indices[:, 2]
-        attributes = alpha[i_indices, j_indices, k_indices]
-        
-        attribute_fname = os.path.join(dataset_folder, attribute_folder, "sihwat")
-        np.save(attribute_fname, attributes)
+        pbar = tqdm(desc=f'store attributes: ', total=point_indices.shape[0])
+    comm.Barrier()
+
+    i_indices, j_indices, k_indices = point_indices[:, 0], point_indices[:, 1], point_indices[:, 2]
+    attributes = alpha[i_indices, j_indices, k_indices]
+
+    for index in range(attributes.shape[0]):
+        if index % size == rank:
+            attribute_fname = os.path.join(dataset_folder, attribute_folder, material_name, f"{material_name}__{index + 1}")
+            np.save(attribute_fname, attributes[index])
+
+            if rank == 0:
+                value = size
+                if  attributes.shape[0] - index < value:
+                    value = attributes.shape[0] - index
+                pbar.update(value)
+
+    comm.Barrier()
+    if rank == 0:
+        pbar.close()
+
