e4233426086d786d962cd14cfbdd4dff3557ed05
Jiawei Zhan <jiaweiz@midway3-login4.rcc.local>
2024-04-13 16:46:49 -0500

    torch gaussian3d_helper

--- qcat/utils/gpu_kernels.py
+++ qcat/utils/gpu_kernels.py
@@ -1,53 +1,49 @@
-USE_GPU = True
-try:
-    import cupy as np
-    try:
-        USE_GPU = np.cuda.is_available()
-    except:
-        USE_GPU = False
-except ImportError:
-    USE_GPU = False
-
-if not USE_GPU:
-    import numpy as np
-import numpy as numpy
+import torch
+import math
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def gaussian3d(unit_cell,      # [3, 3]
-               r1: numpy.ndarray, # [ngrid, 3] in relative scale
-               r2: numpy.ndarray, # [nefield // 2, nmlwf, 3] in relative scale
-               spread: numpy.ndarray, # [nefield //2, nmlwf, 1] in bohr unit
-               dspl_norm: numpy.ndarray, # [nefield // 2, nmlwf, 1] in bohr unit
+               r1,             # [ngrid, 3] in relative scale
+               r2,             # [nefield // 2, nmlwf, 3] in relative scale
+               spread,         # [nefield //2, nmlwf, 1] in bohr unit
+               dspl_norm,      # [nefield // 2, nmlwf, 1] in bohr unit
                ):
     '''
     return shape: [nefield // 2, ngrid]
     FIXME: this function is not optimized, GPU acceleration per grid is possible.
     '''
-    if not USE_GPU:
-        return gaussian3d_helper(unit_cell, r1, r2, spread, dspl_norm)
-    else:
-        unit_cell = np.asarray(unit_cell)
-        r2 = np.asarray(r2)
-        spread = np.asarray(spread)
-        dspl_norm = np.asarray(dspl_norm)
+    with torch.no_grad():
+        unit_cell = torch.from_numpy(unit_cell)
+        r1 = torch.from_numpy(r1)
+        r2 = torch.from_numpy(r2)
+        spread = torch.from_numpy(spread)
+        dspl_norm = torch.from_numpy(dspl_norm)
+        if not torch.cuda.is_available():
+            return gaussian3d_helper(unit_cell, r1, r2, spread, dspl_norm)
+        else:
+            unit_cell = unit_cell.to(device)
+            r2 = r2.to(device)
+            spread = spread.to(device)
+            dspl_norm = dspl_norm.to(device)
 
-        free_memory, _ = np.cuda.runtime.memGetInfo()
-        ngrid = r1.shape[0]
-        nefield, nmlwf, _ = r2.shape
-        nr1 = free_memory // (r1.itemsize * nefield * nmlwf * 3 * 10)
-        lenr1 = (ngrid - 1) // nr1 + 1
-        result = []
-        for i in range(lenr1):
-            low = i * nr1
-            high = min(ngrid, (i + 1) * nr1)
-            r1_gpu = np.asarray(r1[low : high])
-            result.append(gaussian3d_helper(unit_cell, r1_gpu, r2, spread, dspl_norm))
-        return np.concatenate(result, axis=-1).get()
+            free_memory, _ = torch.cuda.mem_get_info()
+            ngrid = r1.shape[0]
+            nefield, nmlwf, _ = r2.shape
+            nr1 = free_memory // (r1.itemsize * nefield * nmlwf * 3 * 10)
+            lenr1 = (ngrid - 1) // nr1 + 1
+            result = []
+            for i in range(lenr1):
+                low = i * nr1
+                high = min(ngrid, (i + 1) * nr1)
+                r1_gpu = r1[low : high].to(device)
+                result.append(gaussian3d_helper(unit_cell, r1_gpu, r2, spread, dspl_norm))
+            return torch.cat(result, dim=-1).cpu().numpy()
 
-def gaussian3d_helper(unit_cell,      # [3, 3]
-                      r1: np.ndarray, # [ngrid, 3] in relative scale
-                      r2: np.ndarray, # [nefield // 2, nmlwf, 3] in relative scale
-                      spread: np.ndarray, # [nefield //2, nmlwf, 1] in bohr unit
-                      dspl_norm: np.ndarray, # [nefield // 2, nmlwf, 1] in bohr unit
+def gaussian3d_helper(unit_cell: torch.Tensor,      # [3, 3]
+                      r1: torch.Tensor,             # [ngrid, 3] in relative scale
+                      r2: torch.Tensor,             # [nefield // 2, nmlwf, 3] in relative scale
+                      spread: torch.Tensor,         # [nefield //2, nmlwf, 1] in bohr unit
+                      dspl_norm: torch.Tensor,      # [nefield // 2, nmlwf, 1] in bohr unit
                       ):
     '''
     return shape: [nefield // 2, ngrid]
@@ -55,7 +51,7 @@ def gaussian3d_helper(unit_cell,      # [3, 3]
     '''
     diff = r1[None, :, None, :] - r2[:, None, :, :] # [nefield // 2, ngrid, nmlwf, 3]
     diff = (diff + 0.5) % 1 - 0.5
-    diff_norm = np.linalg.norm(diff @ unit_cell, axis=-1) # [nefield // 2, ngrid, nmlwf]
-    dspl_norm = dspl_norm.transpose(0, 2, 1)                   # [nefield // 2, 1, nmlwf]
-    spread = spread.transpose(0, 2, 1)                   # [nefield // 2, 1, nmlwf]
-    return np.sum(dspl_norm * np.exp(-0.5 * diff_norm ** 2 / spread ** 2) / ((2 * np.pi * spread ** 2) ** 1.5), axis=-1) # [nefield // 2, ngrid]
+    diff_norm = torch.linalg.norm(diff @ unit_cell, dim=-1) # [nefield // 2, ngrid, nmlwf]
+    dspl_norm = dspl_norm.permute(0, 2, 1)                   # [nefield // 2, 1, nmlwf]
+    spread = spread.permute(0, 2, 1)                   # [nefield // 2, 1, nmlwf]
+    return torch.sum(dspl_norm * torch.exp(-0.5 * diff_norm ** 2 / spread ** 2) / ((2 * math.pi * spread ** 2) ** 1.5), dim=-1) # [nefield // 2, ngrid]
